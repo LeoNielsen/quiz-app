ce75ed806eb6637a6a122bdf0bb96adbea35b556
MieThorberg <cph-mt373@cphbusiness.dk>
2022-03-29 19:45:16 +0200

    made dev branch

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,6 +4,7 @@ import logo from '../img/logo192.png'
 const Header = () => {
   return (
     <header className='header'>
+      {/*Testing*/}
         {/* replace logo */}
         <img src={logo}/>
         <h1>Quiz Name</h1>

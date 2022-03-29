a72412e6e5f0c083eb8aec2d1c9651ea21735111
LeoNielsen <leo.nielsen21@gmail.com>
2022-03-29 13:30:13 +0200

    fetch categories from the web

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import Header from "component/Header"
 import GetQuiz from "component/GetQuiz"
+import react, {useState, useEffect} from "react";
 import "styles/Header.css"
 
-
 export default function App() {
+
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+      const getCategories = async () => {
+          const categoriesFromServer = await fetchCategories();
+          setCategory(categoriesFromServer.trivia_categories)
+      }
+      getCategories();
+  },[])
+
+  const fetchCategories = async () => {
+      const res = await fetch('https://opentdb.com/api_category.php');
+      const data = await res.json();
+      return data;
+  }
+
   return <div className="container">
   <Header />
-  <GetQuiz />
+  <GetQuiz Categories={category} />
   </div>
 }
+
+
--- src/component/GetQuiz.js
+++ src/component/GetQuiz.js
@@ -1,32 +1,46 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const GetQuiz = () => {
+const GetQuiz = ({ Categories }) => {
   return (
     <div className='body'>
         <form>
             <label>Category</label><br/>
-            <select placeholder='category'>
-                <option></option>
+            <select>
+                <option key={0}>Any Category</option>
+                {Categories.map( (cate) => {
+                 return <option key={cate.id}>{cate.name}</option>
+                })
+                }
             </select> <br/><br/>
 
             <label>Number Of Questions</label><br/>
-            <input type='number' min='1' max='50' value={10}/><br/><br/>
+            <input type='number' min='1' max='50' defaultValue={10} /><br/><br/>
 
             <label>Select Difficulty</label><br/>
             <select>
-                <option></option>
+                <option>Any Difficulty</option>
+                <option>Easy</option>
+                <option>Medium</option>
+                <option>Hard</option>
+
             </select><br/><br/>
 
             <label>Select Answer Type</label><br/>
             <select>
-                <option>
-                </option>
+                <option>Any Type</option>
+                <option>Multiple Choice</option>
+                <option>True / False</option>
             </select><br/>
 
 
         </form>
     </div>
   )
+}
+
+GetQuiz.proptype = {
+    Categories: PropTypes.array.isRequired,
 }
 
 export default GetQuiz
